9208a2c04b778804c0f7a500887b919089837e7e
leandrosilvacampos <1411471@sga.pucminas.br>
2023-07-29 17:00:51 -0300

    Atualiza SQL inicial

--- seed.sql
+++ seed.sql
@@ -26,6 +26,12 @@ VALUES
         'Centro'
     );
 
+-- Agreements
+INSERT INTO
+    agreement (id, name, discountType, discountValue, companyId)
+VALUES
+    (1, NULL, 'Particular', 'absolute', 0.00, 1);
+
 -- Especialties
 INSERT INTO
     specialty (id, name)
@@ -72,11 +78,48 @@ INSERT INTO
 VALUES
     (1, '3314', 1);
 
--- Person roles
+-- Patient
+INSERT INTO
+    person (
+        id,
+        name,
+        companyName,
+        fantasyName,
+        taxDocument,
+        email,
+        type,
+        gender,
+        companyId
+    )
+VALUES
+    (
+        2,
+        'Carlos Henrique',
+        '',
+        '',
+        '054.698.569-50',
+        'joao.miguel.@email.com',
+        'individual',
+        'male',
+        1
+    );
+
+INSERT INTO
+    patient (id, personId)
+VALUES
+    (1, 2);
+
+-- Doctor role
 INSERT INTO
     personRole (personId, roleId)
 VALUES
     (1, 1);
+
+-- Patient role
+INSERT INTO
+    personRole (personId, roleId)
+VALUES
+    (2, 2);
 
 -- Procedures
 INSERT INTO
